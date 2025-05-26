035b259c982908fcfae571859975b1fdac91e64c
Fauzan Akmal Mahdi <fauzan.akmal@sprintasia.co.id>
2025-05-26 16:39:37 +0700

    first setup flutter module via cocoapods

---
+++ Pods/Target Support Files/Pods-sample_application2-sample_application2UITests/Pods-sample_application2-sample_application2UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sample_application2_sample_application2UITests : NSObject
+@end
+@implementation PodsDummy_Pods_sample_application2_sample_application2UITests
+@end
---
+++ Pods/Target Support Files/safe_device/safe_device-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_safe_device : NSObject
+@end
+@implementation PodsDummy_safe_device
+@end
